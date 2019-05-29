2b4019ee555a693513268480530838e25caf2293
Camilo Payan <camilo@Camilos-MacBook-Pro-2.local>
2019-05-29 16:33:42 -0400

    Sets the form up along with the json unmarshaling for it.

--- main.go
+++ main.go
@@ -1,14 +1,33 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
+type Form struct {
+	Name    string `json:"name"`
+	Email   string `json:"email"`
+	Message string `json:"message"`
+}
+
+type Payload struct {
+	Form Form `json:"data"`
+}
+
+type Body struct {
+	Payload Payload `json:"payload"`
+}
+
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	// Get information from the data payload.
+	var body Body
+	json.Unmarshal([]byte(request.Body), &body)
+
 	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
-		Body:       "Hello AWS Lambda and Netlify",
+		Body:       "Success!",
 	}, nil
 }
 
