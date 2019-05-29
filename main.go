f5c05fee398354d63593d443260d8e5649f9ccf6
Camilo Payan <camilo@Camilos-MacBook-Pro-2.local>
2019-05-29 17:17:20 -0400

    Adds mailing code.

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/go-gomail/gomail"
+	"net/mail"
 )
 
 type Form struct {
@@ -25,9 +28,30 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 	var body Body
 	json.Unmarshal([]byte(request.Body), &body)
 
+	// Get emails
+	port, _ := strconv.Atoi(os.Getenv("MAIL_PORT"))
+	host := os.Getenv("MAIL_HOST")
+	senderemail := os.Getenv("MAIL_SENDER_EMAIL")
+	senderidentity := os.Getenv("MAIL_SENDER_NAME")
+	smtppassword := os.Getenv("MAIL_PASSWORD")
+	smtpuser := os.Getenv("MAIL_USERNAME")
+	myEmail := os.Getenv("MY_EMAIL")
+
+	// Send email
+	m := gomail.NewMessage()
+	m.SetHeader("From", myEmail)
+	m.SetHeader("To", body.Payload.Form.Email)
+	m.SetHeader("Subject", fmt.Sprintf("%s, I received your email!", body.Payload.Form.Name))
+
+	m.SetBody("text/plain", body.Payload.Form.Message)
+
+	d := gomail.NewDialer(server, port, smtpuser, smtppassword)
+
+	d.DialAndSend(m)
+
 	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
-		Body:       "Success!",
+		Body:       "success!",
 	}, nil
 }
 
