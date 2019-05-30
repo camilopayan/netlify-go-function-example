ea060756487ba13014c9b3b8c5a888a668a13241
Camilo Payan <camilo@Camilos-MacBook-Pro-2.local>
2019-05-30 14:09:04 -0400

    Fixes some broken references.

--- main.go
+++ main.go
@@ -5,8 +5,9 @@ import (
 	"fmt"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/go-gomail/gomail"
-	"net/mail"
+	"github.com/go-mail/mail"
+	"os"
+	"strconv"
 )
 
 type Form struct {
@@ -32,20 +33,19 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 	port, _ := strconv.Atoi(os.Getenv("MAIL_PORT"))
 	host := os.Getenv("MAIL_HOST")
 	senderemail := os.Getenv("MAIL_SENDER_EMAIL")
-	senderidentity := os.Getenv("MAIL_SENDER_NAME")
 	smtppassword := os.Getenv("MAIL_PASSWORD")
 	smtpuser := os.Getenv("MAIL_USERNAME")
-	myEmail := os.Getenv("MY_EMAIL")
 
 	// Send email
-	m := gomail.NewMessage()
-	m.SetHeader("From", myEmail)
+	m := mail.NewMessage()
+	m.SetHeader("From", senderemail)
+	m.SetHeader("Bcc", senderemail)
 	m.SetHeader("To", body.Payload.Form.Email)
 	m.SetHeader("Subject", fmt.Sprintf("%s, I received your email!", body.Payload.Form.Name))
 
 	m.SetBody("text/plain", body.Payload.Form.Message)
 
-	d := gomail.NewDialer(server, port, smtpuser, smtppassword)
+	d := mail.NewDialer(host, port, smtpuser, smtppassword)
 
 	d.DialAndSend(m)
 
